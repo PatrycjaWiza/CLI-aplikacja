84eb599daf582a95aab41b8c538148f66207e003
PatrycjaWiza <95050810+PatrycjaWiza@users.noreply.github.com>
2022-10-22 20:34:33 +0200

    refrences

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CLI-aplikacja
+
+References to screenshots of console logs:
+
+    removeContact https://ibb.co/qpnRhgm
+    addContact https://ibb.co/WsyxvW1
+    getContactById https://ibb.co/P5jkp2R
+    listContacts https://ibb.co/tXPcpt1
--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 // imported module with functions
 const logger = require("./contacts");
+
 // yargs package for easier console logging functions
-const argv = require("yargs").argv;
+// const argv = require("yargs").argv;
+
+// alternative for yargs
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
